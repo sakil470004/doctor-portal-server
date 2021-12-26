ee4e347e093b733ade742b0d419f6625a73a5bb0
Mynul Islam <sk470004@gmail.com>
2021-12-26 18:50:10 +0600

    update filter date and email

--- index.js
+++ index.js
@@ -6,17 +6,38 @@ require("dotenv").config();
 const port = process.env.PORT || 5000
 
 app.use(cors())
+// for the access userData body data
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.poyqe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function run() {
-    try{
+    try {
         await client.connect()
-        console.log('database Connected Successfully');
-        
-        
-    }finally{
+        const database = client.db('doctors_portal');
+        const appointmentCollection = database.collection('appointments');
+
+        // get cursor data
+        app.get('/appointments', async (req, res) => {
+
+            const email = req.query.email;
+            const date = new Date(req.query.date).toLocaleDateString();
+            const query = { email: email ,date:date}
+            // console.log(query)
+            const cursor = appointmentCollection.find(query);
+            const appointments = await cursor.toArray();
+            res.json(appointments);
+        })
+        // insert a appointment
+        app.post('/appointments', async (req, res) => {
+            const appointment = req.body;
+            const result = await appointmentCollection.insertOne(appointment);
+            // console.log(result);
+            res.json(result)
+        })
+
+    } finally {
         // await client.close()
     }
 }
@@ -30,3 +51,11 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`this feaking app listening http://localhost:${port}`)
 })
+
+// app.get('/users')
+// app.post('/users')
+// app.get('/users/:id')
+// app.put('/users/:id');
+// app.delete('/users/:id')
+// users: get
+// users: post
